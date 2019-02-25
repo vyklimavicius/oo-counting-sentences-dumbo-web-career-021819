89b4e975cf7a93ba9b208a717e52a341e621e957
Vytautas Klimavicius <vyklimavicius@hotmail.com>
2019-02-25 10:55:46 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,29 @@
-require 'pry'
+# require 'pry'
 
 class String
 
   def sentence?
-    
+    self.include?(".")
   end
 
   def question?
-
+    self.include?("?")
   end
 
   def exclamation?
-
+    self.include?("!")
   end
 
   def count_sentences
-
+    if self.include?(".") && self.include?(",")
+      self.split(",").length
+    else
+      self.lines(" ").length
+    end
   end
 end
+
+# string = String.new
+# # string = "one. two. three?"
+# p string =  "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+# p string.count_sentences
